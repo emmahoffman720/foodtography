4c4f2cb3ab58bb13c28d70ff946c6f5c7150b1a7
Emma Hoffman <emmahoffman720@gmail.com>
2016-05-12 15:52:04 +0000

    Added validations

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -18,12 +18,18 @@ RSpec.describe PostsController, type: :controller do
 
   describe "posts#create action" do
     it "should successfully create a new post in database" do
-      post :create, gram: {message: 'First Recipe', recipe: 'Bread, peanut butter, jelly'}
+      post :create, post: {message: 'First Recipe', recipe: 'Bread, peanut butter, jelly'}
       expect(response).to redirect_to root_path
 
       post = Post.last
       expect(post.message).to eq("First Recipe")
       expect(post.recipe).to eq("Bread, peanut butter, jelly")
+    end
+
+    it "should successfully deal with validation errors" do
+      post :create, post: {message: ''}
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(Post.count).to eq 0
     end
   end
 
